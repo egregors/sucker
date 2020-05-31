ac3baab2e1b92961503d8466478b9c1c7daff507
egregors <root@egregors.com>
2020-05-31 14:35:03 +1000

    fix: remove old refs

--- app/main.go
+++ app/main.go
@@ -8,25 +8,7 @@ import (
 	"os"
 )
 
-// todo:
-//  - [x] мульти бары для загрузок
-//  - [x] удалять бары, когда загрузилось
-// 	- [ ] ловить interrupt и удалять недокаченое
-//  - [ ] выводить в поплог когда максимальное количество ретраев привышено
-//  - [ ] попрообвать сделать дозагрузку файла на баре после ретрая
-//  - [ ] все таки как то удалять недокаченные файлы
-//  - [x] придумать как сделать общий прогресс бар для всего количества закачек
-//
-//  все еще не понятно, что делать при ктрл - с
-
 func main() {
-	// 1. get CLI args
-	// 2. get URLs list
-	// 3. create wg, progress, context
-	// 4. create downloader
-	// 5. start downloading
-	// 6. catch interrupt
-
 	// get all pages licks from CLI args
 	pageLinks, err := internal.ParseArgs(os.Args[1:])
 	if err != nil {
