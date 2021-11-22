b16f5f754f131d23e9b530de6db8d2581fcda0e7
egregors <root@egregors.com>
2021-11-22 22:20:11 +0300

    end: ?

--- app/main.go
+++ app/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
-	"github.com/egregors/sucker/app/downloader"
-	"github.com/egregors/sucker/app/internal"
 	"log"
 	"os"
+
+	"github.com/egregors/sucker/app/downloader"
+	"github.com/egregors/sucker/app/internal"
 )
 
+// FIXME: Soooo..... locks like we can't make requests through Cloudflare "check" page.
+// 	Instead of page with content we got stub page with JS, and can't jump next.
+// 	For now we got HTTP/1.1 401 Unauthorized by Cloudflare validation.
+//  Looks like this is the end :\
 func main() {
 	// get all pages licks from CLI args
 	pageLinks, err := internal.ParseArgs(os.Args[1:])
@@ -15,6 +20,7 @@ func main() {
 		log.Fatalf("bad args: %v", err)
 	}
 
+	// FIXME: wtf?
 	ctx, _ := context.WithCancel(context.Background())
 
 	d, err := downloader.NewDownloader(ctx, pageLinks)
